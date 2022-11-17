89906b91f6d5b94bab16ff5d60ad8301a794d4ec
jasen3644 <jasen3644@naver.com>
2022-11-17 21:34:43 +0900

    dadad

--- spring_04_mvc/src/main/java/part04/HelloCommandcn.java
+++ spring_04_mvc/src/main/java/part04/HelloCommandcn.java
@@ -1,14 +1,29 @@
 package part04;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloCommandcn {
 	
 	
-	@RequestMapping(value= "/mem.htm")
+	@RequestMapping(value= "/mem.htm",method = RequestMethod.GET)
 	public String form() {
 		return "part04/memfrom";
+	}
+	
+	/*
+	 * @RequestMapping(value= "/mem.htm", method = RequestMethod.POST) public
+	 * ModelAndView submit(String name, int age, ModelAndView mav) {
+	 * mav.addObject("name", name); mav.addObject("age", age);
+	 * mav.setViewName("part04/memPro"); return mav; }
+	 */
+	
+	@PostMapping("/mem.htm")
+	public String submit(MemDTO dat) {
+		return "part04/memPro";
 	}
 }
